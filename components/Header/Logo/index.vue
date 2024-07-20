ca71664a3799f2885f223addb6d6a35cf6570b7f
Stepan Leonov <s.leonov@innopolis.university>
2024-07-20 23:35:59 +0300

    config store and logo for mobile

--- components/Header/Logo/index.vue
+++ components/Header/Logo/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="logo">
-      <img v-if="colorMode.value === 'light'" src="~/assets/image.png" alt="logo" :class="config.getMobile ? 'h-8' : 'h-10'">
-      <img v-if="colorMode.value === 'dark'" src="~/assets/image-w.png" alt="logo" :class="config.getMobile ? 'h-8' : 'h-10'">
+      <img v-if="colorMode.value === 'light'" src="~/assets/image.png" alt="logo" :class="config.getMobile ? 'h-6' : 'h-10'">
+      <img v-if="colorMode.value === 'dark'" src="~/assets/image-w.png" alt="logo" :class="config.getMobile ? 'h-6' : 'h-10'">
     </div>
 </template>
 
