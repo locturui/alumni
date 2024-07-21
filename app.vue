41c84f6e631e97e289bc7a1872a9ed34be1b0c41
Stepan Leonov <s.leonov@innopolis.university>
2024-07-21 17:10:55 +0300

    user data fetch on reload

--- app.vue
+++ app.vue
@@ -6,3 +6,12 @@
     </NuxtLayout>
   </div>
 </template>
+
+<script setup>
+import {useAuthStore} from "~/stores/authStore.js";
+
+const auth = useAuthStore()
+onBeforeMount(() => {
+  auth.validate()
+})
+</script>
