fdb77d37c42b2558f4f5b0767f4be8910b989cbb
Stepan Leonov <s.leonov@innopolis.university>
2024-07-22 05:35:04 +0300

    await smth idk

--- components/Ranking.vue
+++ components/Ranking.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col sm:flex-row justify-between items-center">
-    <div v-for="donator in donators" :key="donator.id" class="flex items-center bg-white shadow-md rounded-lg p-4 m-2">
+    <div v-for="donator in donators" :key="donator.ID" class="flex items-center bg-white shadow-md rounded-lg p-4 m-2">
       <div class="relative">
         <div class="icon absolute -top-4 -left-4 bg-white text-white font-ibm rounded-full w-8 h-8 flex items-center justify-center">
           <font-awesome-icon :icon="['fas', 'medal']" class="w-full h-full object-contain" style="color: #40BA21;" />
@@ -10,24 +10,24 @@
         </div>
       </div>
       <div class="ml-4">
-        <div class=" font-ibm text-[#40BA21] font-semibold">{{ donator.name }}</div>
-        <div class="text-gray-500 font-ibm" >{{ donator.amount }} roubles</div>
+        <div class=" font-ibm text-[#40BA21] font-semibold">{{ donator.User.Name }}</div>
+        <div class="text-gray-500 font-ibm" >{{ donator.Amount }} roubles</div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-const res = await fetch('https://api.alumni-portal.ru')
-
-const donators = [
-  { id: 1, name: "Donater Full Name", amount: 200, avatar: "" },
-  { id: 2, name: "Donater Full Name", amount: 200, avatar: "" },
-  // Add more donators as needed
-];
+defineProps(['donators'])
+// const donators = [
+//   { id: 1, name: "Donater Full Name", amount: 200, avatar: "" },
+//   { id: 2, name: "Donater Full Name", amount: 200, avatar: "" },
+//   // Add more donators as needed
+// ];
 </script>
 
 <style lang="sass" scoped>
 .icon
     border: 1px solid #A9A9A9
+
 </style>
