1624981226e64d3deebfbb3ec02edd669926783c
Stepan Leonov <s.leonov@innopolis.university>
2024-07-22 03:23:56 +0300

    fetch projects

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,11 +1,10 @@
 <template>
   <div class="p-4 mt-[6rem]">
     <h1 class="text-4xl font-bold text-center font-montserrat" style="color: #40BA21">All Projects</h1>
-<!--    <div class="mt-7 container flex flex-wrap gap-5 justify-around mx-auto">-->
-<!--      <UICard v-for="project in projects" :key="project.ID" image="/inno.png" :header="project.Name"-->
-<!--        :description="project.Description" :project="project" @clicked="(id) => clickHandle(id)"/>-->
-<!--    </div>-->
-    {{ projects }}
+    <div class="mt-7 container flex flex-wrap gap-5 justify-around mx-auto">
+      <UICard v-for="project in projects" :key="project.ID" image="/inno.png" :header="project.Name"
+        :description="project.Description" :project="project" @clicked="(id) => clickHandle(id)"/>
+    </div>
   </div>
 </template>
 
