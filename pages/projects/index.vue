eb37c8cce51f61e066f00cee6485cc6e9af38967
Stepan Leonov <s.leonov@innopolis.university>
2024-07-22 03:50:26 +0300

    fetch projects

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -2,8 +2,8 @@
   <div class="p-4 mt-[6rem]">
     <h1 class="text-4xl font-bold text-center font-montserrat" style="color: #40BA21">All Projects</h1>
     <div class="mt-7 container flex flex-wrap gap-5 justify-around mx-auto">
-      <UICard v-for="project in projects" :key="project.ID" image="/inno.png" :header="project.Name"
-        :description="project.Description" :project="project" @clicked="(id) => clickHandle(id)"/>
+      <UICard v-for="project in projects" :key="project.ID" :image="project.url" :header="project.title"
+              :description="project.desc" :project="project" @clicked="(ID) => clickHandle(ID)"/>
     </div>
   </div>
 </template>
@@ -13,18 +13,15 @@
 <script setup>
 import { useProjectStore } from '/stores/projectStore.js'
 const store = useProjectStore()
-
-onBeforeMount(() => {
-  store.fetchProjects()
-})
-
-const { projects } = storeToRefs(store)
+await store.fetchProjects()
 const router = useRouter();
 const clickHandle = (id) => {
   console.log(id)
   router.push('/projects/' + id)
 }
 
+
+const projects = store.getProjects
 </script>
 
 <style lang="sass" scoped>
